bcc5f16b7cf4924353297a208a5b89d734515171
Ninfa-C <ninfa_95@hotmail.it>
2024-11-14 18:14:57 +0100

    end

--- D4/assets/js/script.js
+++ D4/assets/js/script.js
@@ -1,15 +1,15 @@
 const tombola = document.getElementById("tombola");
-const btnEstrai = document.getElementById('estrai')
-const btnAgain = document.getElementById('again')
+const btnEstrai = document.getElementById("estrai");
+const btnAgain = document.getElementById("again");
 const nExtract = [];
 
 document.addEventListener("load", init());
 
 function init() {
   cellCreation();
-  estrazione();
+  end();
   //startPlaying()
-  again()
+  again();
 }
 
 function cellCreation() {
@@ -23,44 +23,44 @@ function cellCreation() {
   }
 }
 
+function end() {
+  btnEstrai.addEventListener("click", function () {
+    if (nExtract.length === 90) {
+      btnEstrai.setAttribute('disabled', 'true');
+      btnEstrai.classList.add('not')
+      return;
+    } else estrazione();
+  });
+}
 
 function estrazione() {
-    btnEstrai.addEventListener('click', function() {
-        if (nExtract.length>=90){
-            btnEstrai.setAttribute('disabled', 'true')
-            return;
-        }
-        let extract;     
-        do {
-            extract = Math.floor(Math.random() * 90) + 1;
-        } while (nExtract.includes(extract));
-        nExtract.push(extract);
-        console.log (nExtract)
-        select()
-    });
+  let extract;
+  do {
+    extract = Math.floor(Math.random() * 90) + 1;
+  } while (nExtract.includes(extract));
+  nExtract.push(extract);
+  console.log(nExtract);
+  select();
 }
 
 function select() {
-    let cell = document.querySelectorAll('#tombola div p');
-    for (let i = 0; i < cell.length; i++) {
+  let cell = document.querySelectorAll("#tombola div p");
+  for (let i = 0; i < cell.length; i++) {
     let cellNumber = parseInt(cell[i].innerText);
     if (nExtract.includes(cellNumber)) {
-        cell[i].parentElement.classList.add('select');
-        }
+      cell[i].parentElement.classList.add("select");
     }
+  }
 }
 
-
-function again(){
-    btnAgain.addEventListener('click', function() {
-    nExtract.splice(0, nExtract.length)
-    let cell = document.querySelectorAll('#tombola div p');
-    cell.forEach(cell => {
-        cell.parentElement.classList.remove('select'); 
-})
-})}
-
-
-
-
-
+function again() {
+  btnAgain.addEventListener("click", function () {
+    nExtract.splice(0, nExtract.length);
+    let cell = document.querySelectorAll("#tombola div p");
+    cell.forEach((cell) => {
+      cell.parentElement.classList.remove("select");
+    });
+    btnEstrai.removeAttribute('disabled')
+    btnEstrai.classList.remove('not')
+  });
+}
